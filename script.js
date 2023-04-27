2a1688c7da404f1cde254c560699cb1c07184ae3
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-27 16:37:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+// function Sayhello() {
+// 	return new promise((resolve) =>{
+// 	setTimeout(() =>{
+// 		resolve("Hello, world");
+// 	},1000)
+// })
+
+// }
+// let div = document.getElementById("output")
+// Sayhello().then((data)=>{
+// 	div.textContent = data;
+// })
+
+
+function sayHello(){
+	return new promise((resolve) => {
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	});
+}
+
+const output =document.getElementbyId("output");
+sayHello().then((result)=>{
+	output.textContent=result;
+});
+
+
